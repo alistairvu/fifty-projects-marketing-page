c8af7647ad69f2d374518b2dba74adb34578e8a7
Alistair Vu <alistairvu@gmail.com>
2021-03-09 23:39:22 +0700

    Fix forwardRef error

--- components/AppButton.tsx
+++ components/AppButton.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react"
 import Button from "react-bootstrap/Button"
 
 interface AppButtonProps {
@@ -9,24 +10,23 @@ interface AppButtonProps {
   disabled?: boolean
 }
 
-export const AppButton = ({
-  color,
-  onClick,
-  title,
-  gradient,
-  submit,
-  disabled,
-}: AppButtonProps) => {
-  return (
-    <Button
-      className={gradient ? "gradient" : ""}
-      style={{ backgroundColor: color, border: "none" }}
-      variant="dark"
-      onClick={onClick}
-      type={submit && "submit"}
-      disabled={disabled}
-    >
-      {title}
-    </Button>
-  )
-}
+export const AppButton = forwardRef(
+  (
+    { color, onClick, title, gradient, submit, disabled }: AppButtonProps,
+    ref: any
+  ) => {
+    return (
+      <Button
+        className={gradient ? "gradient" : ""}
+        style={{ backgroundColor: color, border: "none" }}
+        variant="dark"
+        onClick={onClick}
+        type={submit && "submit"}
+        disabled={disabled}
+        ref={ref}
+      >
+        {title}
+      </Button>
+    )
+  }
+)
