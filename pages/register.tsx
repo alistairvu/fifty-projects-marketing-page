880c5c68abf1487204338e32fd58cd8bac3fb737
Alistair Vu <alistairvu@gmail.com>
2021-03-09 16:08:52 +0700

    Add head to register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,10 +1,20 @@
 import Container from "react-bootstrap/Container"
+import Head from "next/head"
 
 const Register = () => {
   return (
-    <Container className="text-center py-3">
-      <h1 className="gradient gradient-text bold-text">Welcome to the club!</h1>
-    </Container>
+    <>
+      <Head>
+        <title>ODD EYE CIRCLE | Register</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container className="text-center py-3">
+        <h1 className="gradient gradient-text bold-text">
+          Welcome to the club!
+        </h1>
+      </Container>
+    </>
   )
 }
 
